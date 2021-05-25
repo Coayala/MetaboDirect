315c06a9c28e139986a3a9bee0643f4bc84514bf
Coayala <christian_jpg2@hotmail.com>
2021-05-25 12:46:36 -0700

    Norm methods and KEGG analysis

--- run_metabodirect.py
+++ run_metabodirect.py
@@ -10,11 +10,14 @@ import os
 import datetime
 import subprocess
 import glob
+import sys
+import time
 from itertools import combinations
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import py4cytoscape as p4c
 from shutil import which
 from platform import system
 from functions_metabodirect import *
@@ -25,7 +28,7 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Program will run all the MetaboDirect analysis pipeline',
+        description='Program for running all the MetaboDirect analysis pipeline',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('data_file',
@@ -34,7 +37,7 @@ def get_args():
                         type=str)
 
     parser.add_argument('metadata_file',
-                        help='Name of the fie with the sample information (metadata) in tabular format',
+                        help='Name of the file with the sample information (metadata) in tabular format',
                         metavar='METADATA',
                         type=str)
 
@@ -70,12 +73,60 @@ def get_args():
                         type=str,
                         nargs=2)
 
-    parser.add_argument('-k',
+    parser.add_argument('-b',
                         '--biochem_key',
                         help='File with the biochemical key to use for the transformation network',
                         metavar='STR',
                         type=str,
                         default='Default key')
+
+    parser.add_argument('-t',
+                        '--transformation_analysis',
+                        help='Set this option to perform a transformation netwokr analysis of the samples',
+                        action='store_true',
+                        default=False)
+
+    parser.add_argument('-k',
+                        '--kegg_annotation',
+                        help='Set this option to perform annotation of the molecular formulas using'
+                             'the KEGG database',
+                        action='store_true',
+                        default=False)
+
+    norm = parser.add_argument_group('Normalization methods',
+                                     'Options to define how data normalization will be carried out')
+
+    norm.add_argument('-n',
+                      '--norm_method',
+                      help='Available methods to normalize data',
+                      metavar='STR',
+                      type=str,
+                      choices=['mean', 'median', 'z-score', 'sum', 'max', 'minmax', 'binary', 'none'],
+                      default='max')
+
+    norm.add_argument('--norm_subset',
+                      help='Subset of the data to use for normalization purpouses. LOS uses peaks in the top L '
+                           'order statistics, PPP uses peaks having a minimum percentage of observed values.',
+                      metavar='STR',
+                      type=str,
+                      choices=['ALL', 'LOS', 'PPP'],
+                      default='ALL'
+                      )
+
+    norm.add_argument('--subset_parameter',
+                      help='If using a sample subset for nomalization, this parameter defines the subsample of peaks '
+                           'that will be used for normalization.'
+                           'If not defined, the default values will be 0.3 for LOS and 0.5 for PPP',
+                      metavar='STR',
+                      type=str
+                      )
+
+    norm.add_argument('--log_transform',
+                      help='Set this option to log transform the data. (Program will fail if there are peaks with '
+                           'intensities of 0. Consider tranforming this values into 1 if log transformation is desired',
+                      action='store_true',
+                      default=False
+                      )
 
     args = parser.parse_args()
 
@@ -86,11 +137,17 @@ def get_args():
         parser.error(f'File {args.metadata_file} does not exist')
 
     if len(args.group) > 2:
-        parser.error(f'Incorrect number of variables for grouping. Please enter exactly two values')
+        parser.error('Incorrect number of variables for grouping. Please enter exactly two values')
 
     if args.mass_filter:
         if args.mass_filter[1] < args.mass_filter[0]:
-            parser.error(f'Incorrect order of values for filtering. Please enter min_mz first (e.g. -f 200 900)')
+            parser.error('Incorrect order of values for filtering. Please enter min_mz first (e.g. -f 200 900)')
+
+    if args.norm_subset != 'ALL':
+        if args.subset_parameter > 1:
+            parser.error(f'Incorrect subset parameter. Subset parameter must be between 0 and 1 (proportion).')
+        if not args.subset_parameter:
+            args.subset_parameter = 0.3 if args.norm_subset == 'LOS' else 0.5
 
     return args
 
@@ -169,7 +226,6 @@ def thermo_idx_and_classes(df, path):
 
     df = calculate_ratios(df)
     df = calculate_classes(df)
-    df = normalize_intensities(df)
     df_formulas = df[df['C'] > 0]
 
     filename = os.path.join(path, 'Report_processed.csv')
@@ -180,6 +236,78 @@ def thermo_idx_and_classes(df, path):
     print(f'Report saved as: {filename}')
 
     return df_formulas
+
+
+# --------------------------------------------------
+def data_normalization(df, norm_method, norm_subset, subset_parameter=1, log=False):
+    """Normalize direct injection data based on the specified parameters."""
+
+    samples = get_list_samples(df)
+    input_data = df[samples]
+
+    # Pivoting the data, so it removes the masses that are not present in any of the samples
+    input_data.insert(0, 'Mass', list(df['Mass']))
+    input_data = pd.melt(input_data, id_vars='Mass', value_vars=samples, var_name='SampleID', value_name='Intensity')
+    input_data = input_data[input_data['Intensity'] > 0]
+    input_data = input_data.pivot(index='Mass', columns='SampleID', values='Intensity')
+    npeaks, nsamples = np.shape(input_data)
+
+    if log:
+        assert 0 not in input_data, 'Cannot calculate log 0, please consider changing this values to 1'
+        input_data = np.log(input_data)
+
+    # Perform sample subset to calculate normalization factors
+    if norm_subset == 'PPP':
+        min_observations = int(subset_parameter * nsamples)
+        selected_peaks = []
+        for peak in range(0, npeaks):
+            if sum(input_data.iloc[peak, :] > 0) > min_observations:
+                keep = input_data.iloc[peak, :].name
+                selected_peaks.append(keep)
+    elif norm_subset == 'LOS':
+        ntop_peaks = int(subset_parameter * npeaks)
+        keep = []
+        for sample in range(0, nsamples):
+            ord_peaks = input_data.iloc[:, sample].sort_values(ascending=False)
+            keep.extend(list(ord_peaks.iloc[0:ntop_peaks, ].index))
+        selected_peaks = list(np.unique(keep))
+    else:
+        selected_peaks = list(input_data.index)
+
+    # Calculate normalization factors
+    sample_mean = input_data.loc[selected_peaks, :].mean(axis=0, skipna=True)
+    sample_min = input_data.loc[selected_peaks, :].min(axis=0, skipna=True)
+    sample_max = input_data.loc[selected_peaks, :].max(axis=0, skipna=True)
+    sample_median = input_data.loc[selected_peaks, :].median(axis=0, skipna=True)
+    sample_std = input_data.loc[selected_peaks, :].std(axis=0, skipna=True)
+    sample_sum = input_data.loc[selected_peaks, :].sum(axis=0, skipna=True)
+
+    # Normalize data based on chosen method
+    if norm_method == 'mean':
+        norm_data = (input_data - sample_mean) / (sample_max - sample_min)
+    elif norm_method == 'median':
+        norm_data = (input_data - sample_median) / (sample_max - sample_min)
+    elif norm_method == 'z-score':
+        norm_data = (input_data - sample_mean) / sample_std
+    elif norm_method == 'sum':
+        norm_data = input_data / sample_sum
+    elif norm_method == 'max':
+        norm_data = input_data / sample_max
+    elif norm_method == 'minmax':
+        norm_data = (input_data - sample_min) / (sample_max - sample_min)
+    elif norm_method == 'binary':
+        norm_data = input_data.copy()
+        norm_data[norm_data > 0] = 1
+    else:
+        norm_data = input_data.copy()
+
+    norm_data['Mass'] = norm_data.index
+    norm_data = norm_data.reset_index(drop=True)
+    norm_data = norm_data.replace(np.nan, 0)
+    temp = df[df.columns[~df.columns.isin(samples)]]
+    norm_data = temp.merge(norm_data, on='Mass')
+
+    return norm_data
 
 
 # --------------------------------------------------
@@ -247,7 +375,7 @@ def error_per_group(df, group, path):
     grid = sns.FacetGrid(error,
                          col=group[0],
                          row=None if len(group) == 1 else group[1],
-                         hue=group[1],
+                         hue=group[0],
                          height=5)
 
     grid.map(plt.scatter, "Mass", "mean", s=1)
@@ -259,7 +387,7 @@ def error_per_group(df, group, path):
 
 
 # --------------------------------------------------
-def write_r_script(rscript, outdir, metadata_file, groups):
+def write_r_script(rscript, outdir, metadata_file, groups, norm_method='max'):
     """Function to write R scripts based on the provided Rscripts templates"""
     current_dir = os.getcwd().replace('\\', '/')
     metadata_file = metadata_file.replace('\\', '/')
@@ -275,9 +403,7 @@ def write_r_script(rscript, outdir, metadata_file, groups):
                    .replace('%outdir%', os.path.split(outdir)[0])
                    .replace('%group1%', groups[0])
                    .replace('%group2%', groups[1] if len(groups) == 2 else 'NULL')
-                   .replace('%groups%',
-                            '"{}","{}"'.format(groups[0], groups[1]) if len(groups) == 2 else '"{}"'.format(groups[0])
-                            )
+                   .replace('%norm_method%', norm_method)
                    )
     r_fh.close()
 
@@ -295,8 +421,8 @@ def run_r(rscript):
             r_cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
         out, err = p.communicate()
-        if out:
-            print(out)
+        # if out:
+        #     print(out)
 
         if err:
             print(err)
@@ -350,8 +476,8 @@ def calculate_transformations(df, keys, path):
 
         # make pd df
         result_df = pd.DataFrame(result_tuples, columns=[
-                                    'Feature_X', 'Feature_Y', 'Difference',
-                                    'Group', 'Transformation', 'Formula', 'mf'])
+            'Feature_X', 'Feature_Y', 'Difference',
+            'Group', 'Transformation', 'Formula', 'mf'])
 
         result_df['SampleID'] = sample
 
@@ -394,14 +520,75 @@ def summarize_transformations(path):
 def get_node_table(df, path):
     """Create a node table for the transformation networks"""
     node_table = df[['Mass', 'C', 'H', 'O', 'N', 'S', 'P', 'OC', 'HC', 'NOSC',
-                      'GFE', 'Class', 'Formula', 'Element']].drop_duplicates('Mass')
-
-    node_table['Mass'] = node_table['Mass'].astype(float).apply(lambda x: '%.6f' % x).astype(str)
+                     'GFE', 'Class', 'MolecularFormula', 'El_comp']].drop_duplicates('Mass')
 
     filename = os.path.join(path, 'node_table.csv')
     node_table.to_csv(filename, index=False)
 
     return node_table
+
+
+# --------------------------------------------------
+def create_cytoscape_network(node_table, path):
+    """Create a cytoscape network using the node table"""
+    node_table['Mass'] = round(node_table['Mass'], 4)
+    node_table['Mass'] = node_table['Mass'].astype(str)
+    node_table = node_table.rename(columns={'Mass': 'id'})
+
+    mol_classes = list(np.unique(node_table['Class']))
+    node_colors = sns.color_palette('Set3', len(mol_classes)).as_hex()
+
+    network_stats = []
+    files_path = os.path.join(path, 'transf_by_sample', 'transformations_*.csv')
+    edge_files = glob.glob(files_path)
+
+    for file in edge_files:
+        edge_table = pd.read_csv(file)
+        edge_table['Feature_X'] = round(edge_table['Feature_X'], 4)
+        edge_table['Feature_X'] = edge_table['Feature_X'].astype(str)
+        edge_table['Feature_Y'] = round(edge_table['Feature_Y'], 4)
+        edge_table['Feature_Y'] = edge_table['Feature_Y'].astype(str)
+        edge_table = edge_table.rename(columns={'Feature_X': 'source', 'Feature_Y': 'target'})
+        print(f'Creating transformations network for sample: {edge_table.SampleID.unique()[0]}')
+        with pd.option_context('mode.chained_assignment', None):
+            p4c.create_network_from_data_frames(None, edge_table,
+                                                source_id_list='source',
+                                                target_id_list='target',
+                                                interaction_type_list='Transformation',
+                                                title=edge_table.SampleID.unique()[0],
+                                                collection='Transformation networks')
+            p4c.load_table_data(node_table, data_key_column='id')
+
+        transformation_group = list(np.unique(edge_table['Group']))
+        edge_colors = sns.color_palette('dark', len(transformation_group)).as_hex()
+
+        p4c.set_node_shape_default('ELLIPSE')
+        p4c.set_node_color_mapping('Class', mol_classes, node_colors, mapping_type='d')
+        p4c.set_edge_color_mapping('Group', transformation_group, edge_colors, mapping_type='d')
+
+        network_analyzer = p4c.commands_post('analyzer analyze')
+        network_stats.append(
+            {'SampleID': edge_table.SampleID.unique()[0],
+             'netHE': network_analyzer['heterogeneity'],
+             'nNodes': network_analyzer['nodeCount'],
+             'nEdges': network_analyzer['edgeCount'],
+             'meanNeighbors': network_analyzer['avNeighbors'],
+             'clusterCoeff': network_analyzer['cc'],
+             'netDensity': network_analyzer['density'],
+             'netCentralization': network_analyzer['centralization'],
+             'connectComponents': network_analyzer['ncc'],
+             'pathLength': network_analyzer['avSpl'],
+             'netDiameter': network_analyzer['diameter'],
+             'netRadius': network_analyzer['radius']}
+        )
+
+        time.sleep(10)
+
+    network_stats = pd.DataFrame(network_stats)
+    filename = os.path.join(path, 'network_summary_statistics.csv')
+    network_stats.to_csv(filename, index=False)
+
+    return
 
 
 # --------------------------------------------------
@@ -432,6 +619,7 @@ def main():
 
     df = data_filtering(df, args.mass_filter)
     df = thermo_idx_and_classes(df, path=list_dir[0])
+    df = data_normalization(df, args.norm_method, args.norm_subset, args.subset_parameter, args.log_transform)
     calculate_summaries(df, path=list_dir[0])
     matrix_features = get_matrix(df)
     matrix_features.to_csv(os.path.join(list_dir[0], 'matrix_features.csv'))
@@ -463,6 +651,18 @@ def main():
     run_r(data_exploration_script)
     print(f'Find results and R script in the directory: {os.path.abspath(list_dir[2])}')
 
+    if args.kegg_annotation:
+        print('Starting annotation of molecular formulas using the KEGG database')
+        kegg_annotation_script = write_r_script('KEGG_annotation_template.R', outdir=list_dir[2],
+                                                metadata_file=args.metadata_file if not args.filter_by
+                                                else os.path.join(list_dir[0], 'filtered_metadata.csv'),
+                                                groups=args.group)
+        run_r(kegg_annotation_script)
+        print(f'Find results and R script in the directory: {os.path.abspath(list_dir[2])}')
+    else:
+        print('KEGG annotation not selected. If you wish to perform a KEGG annotation run the script again using'
+              'the -k/--kegg_annotation option')
+
     print('\n------------------------\nData Exploration finished\n------------------------\n')
 
     print('------------------------\nStarting statistical analysis\n------------------------\n')
@@ -477,6 +677,13 @@ def main():
 
     print('------------------------\nStarting transformation network analysis\n------------------------\n')
 
+    df.to_csv('df.csv', index=False)
+
+    if not args.transformation_analysis:
+        print('Transformation analysis not selected. '
+              'If you wish to do a transformation analysis please set the option "-t"')
+        sys.exit()
+
     keys = get_keys(os.path.join(os.path.split(os.path.realpath(__file__))[0],
                                  'data',
                                  'transf_key.csv')) if args.biochem_key == 'Default key' else get_keys(args.biochem_key)
@@ -484,6 +691,31 @@ def main():
     summarize_transformations(path=list_dir[4])
     node_table = get_node_table(df, path=list_dir[4])
 
+    check = ''
+    while check != 'You are connected to Cytoscape!':
+        cytoscape = input(f'Please open Cytoscape and press the ENTER key [q for quit].')
+        if cytoscape == '':
+            try:
+                check = p4c.cytoscape_ping()
+            except (ValueError, Exception):
+                print('Cytoscape is not open.')
+        elif cytoscape == 'q':
+            sys.exit()
+        else:
+            check = 'Cytoscape not open'
+
+    print('Starting network construction on Cytoscape')
+
+    create_cytoscape_network(node_table, path=list_dir[4])
+
+    network_stats_script = write_r_script('network_stats_template.R', outdir=list_dir[4],
+                                          metadata_file=args.metadata_file if not args.filter_by
+                                          else os.path.join(list_dir[0], 'filtered_metadata.csv'),
+                                          groups=args.group, norm_method=args.norm_method)
+
+    print(f'Running R script: {network_stats_script}')
+    run_r(network_stats_script)
+    print(f'Find results and R script in the directory: {os.path.abspath(list_dir[4])}')
 
 
 # --------------------------------------------------
